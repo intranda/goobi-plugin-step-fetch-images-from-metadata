209589d01e9ff50bafbef7d199cdfb98fd38fd5a
Robert Sehr <robert.sehr@intranda.com>
2026-02-19 10:57:21 +0100

    use case insensitive file comparison

--- module-base/src/main/java/de/intranda/goobi/plugins/FetchImagesFromMetadataStepPlugin.java
+++ module-base/src/main/java/de/intranda/goobi/plugins/FetchImagesFromMetadataStepPlugin.java
@@ -641,7 +641,7 @@ public class FetchImagesFromMetadataStepPlugin implements IStepPluginVersion2 {
     private File getMatchedImageFile(String strImage, String folder) {
         String regex = getRegularExpression(strImage);
         List<Path> imagePaths = storageProvider.listFiles(folder,
-                path -> path.getFileName().toString().matches(regex) || path.getFileName().toString().equals(strImage));
+                path -> path.getFileName().toString().matches(regex) || path.getFileName().toString().equalsIgnoreCase(strImage));
 
         // take the first match if there is any
         return imagePaths.isEmpty() ? null : imagePaths.get(0).toFile();
